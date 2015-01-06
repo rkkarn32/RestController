180da9d159a12c2db6c2c32f7e37df1451851e8c
rkkarn32 <rk.karn32@gmail.com>
2015-01-05 22:14:40 -0600

    First commit with Rest Controller

---
+++ src/main/java/ramesh/rest/RestControllers.java
@@ -0,0 +1,20 @@
+package ramesh.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RestControllers {
+	@RequestMapping(value="/rest",method=RequestMethod.GET,produces="application/json")
+	public List<Person> getPerson(){
+		List<Person> list = new ArrayList<Person>();
+		list.add(new Person("Ramesh", 27));
+		list.add(new Person("Deepesh",29));
+		return list;
+	}
+}
